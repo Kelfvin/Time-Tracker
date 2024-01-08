f051c6fa7ededf7cc825f1462f5d2254834fb9e0
Kelfvin <kivenf3776@gmail.com>
2024-01-08 12:27:56 +0800

    完成根据周日完成统计视图的不同挂载

--- lib/page/statistic/view.dart
+++ lib/page/statistic/view.dart
@@ -7,28 +7,59 @@ import 'package:time_tracker/page/statistic/widget/list_chart.dart';
 import 'package:time_tracker/page/statistic/widget/pie_chart.dart';
 
 class StatisticPage extends StatelessWidget {
-  final StatisticPageController controller = Get.put(StatisticPageController());
+  final StatisticPageController statisticPageController =
+      Get.put(StatisticPageController());
 
   StatisticPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView(children: [
-      DateRangePicker(),
-      const Wrap(
-        children: [
-          Card(child: PieChart()),
-          Card(
-            child: ListChart(),
-          ),
-          Card(
-            child: ListTile(
-              title: Text("每周时间统计", style: TextStyle(fontSize: 15)),
-              subtitle: LineChart(),
-            ),
-          ),
-        ],
+    return Obx(() => _buildView());
+  }
+
+  Widget _buildView() {
+    List<Widget> components = [];
+
+    if (statisticPageController.dateRangeMode.value == DateRangeMode.day) {
+      components = _buildDayComponents();
+    } else {
+      components = _buildMultiDayComponents();
+    }
+
+    return ListView(children: [DateRangePicker(), Wrap(children: components)]);
+  }
+
+  List<Widget> _buildDayComponents() {
+    List<Widget> components = [];
+    components.add(
+      const Card(child: PieChart()),
+    );
+    components.add(
+      const Card(
+        child: ListChart(),
       ),
-    ]);
+    );
+
+    return components;
+  }
+
+  List<Widget> _buildMultiDayComponents() {
+    List<Widget> components = [];
+    components.add(
+      const Card(child: PieChart()),
+    );
+    components.add(
+      const Card(
+        child: ListChart(),
+      ),
+    );
+
+    components.add(
+      const Card(
+        child: LineChart(),
+      ),
+    );
+
+    return components;
   }
 }
